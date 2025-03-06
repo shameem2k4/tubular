14ece40a40686e033ef256b7a32ce4cce90ea248
Justin Staerck <51384727+alphanumericmale@users.noreply.github.com>
2025-03-06 14:43:16 +0000

    tests narwhalified

--- tests/imputers/test_ArbitraryImputer.py
+++ tests/imputers/test_ArbitraryImputer.py
@@ -1,6 +1,8 @@
+import narwhals as nw
 import pandas as pd
 import pytest
 
+import tests.utils as u
 from tests.base_tests import (
     ColumnStrListInitTests,
     GenericFitTests,
@@ -12,14 +14,16 @@ from tubular.imputers import ArbitraryImputer
 
 
 # Dataframe used exclusively in this testing script
-def create_downcast_df():
+def create_downcast_df(library):
     """Create a dataframe with mixed dtypes to use in downcasting tests."""
-    return pd.DataFrame(
+    df = pd.DataFrame(
         {
             "a": [1, 2, 3, 4, 5],
             "b": [1.0, 2.0, 3.0, 4.0, 5.0],
         },
     )
+
+    return u.dataframe_init_dispatch(df, library)
 
 
 class TestInit(ColumnStrListInitTests):
@@ -61,40 +65,37 @@ class TestTransform(GenericImputerTransformTests, GenericTransformTests):
     def setup_class(cls):
         cls.transformer_name = "ArbitraryImputer"
 
-    # Unit testing to check if downcast datatypes of columns is preserved after imputation is done
-    def test_impute_value_preserve_dtype(self):
+    @pytest.mark.parametrize("library", ["pandas", "polars"])
+    def test_impute_value_preserve_dtype(self, library):
         """Testing downcast dtypes of columns are preserved after imputation using the create_downcast_df dataframe.
 
-        Explicitly setting the dtype of "a" to int8 and "b" to float16 and check if the dtype of the columns are preserved after imputation.
+        Explicitly setting the dtype of "a" to int8 and "b" to float32 and check if the dtype of the columns are preserved after imputation.
         """
-        df = (
-            create_downcast_df()
-        )  # By default the dtype of "a" and "b" are int64 and float64 respectively
+        df = create_downcast_df(library)
+        df = nw.from_native(df)
+        df = df.with_columns(
+            df["a"].cast(nw.Int8),
+            df["b"].cast(nw.Float32),
+        )
 
         # Imputing the dataframe
         x = ArbitraryImputer(impute_value=1, columns=["a", "b"])
 
-        # Setting the dtype of "a" to int8 and "b" to float16
-        df["a"] = df["a"].astype("int8")
-        df["b"] = df["b"].astype("float16")
+        df_transformed = x.transform(df.to_native())
 
-        # Checking if the dtype of "a" and "b" are int8 and float16 respectively
-        assert df["a"].dtype == "int8"
-        assert df["b"].dtype == "float16"
+        # Convert both DataFrames to a common format using Narwhals
+        df_transformed_common = nw.from_native(df_transformed)
 
-        # Impute the dataframe
-        df = x.transform(df)
-
-        # Checking if the dtype of "a" and "b" are int8 and float16 respectively after imputation
-        assert df["a"].dtype == "int8"
-        assert df["b"].dtype == "float16"
+        # Check if the dtype of "a" and "b" are preserved after imputation
+        assert df_transformed_common["a"].dtype == df["a"].dtype
+        assert df_transformed_common["b"].dtype == df["b"].dtype
 
 
 class TestOtherBaseBehaviour(OtherBaseBehaviourTests):
     """
     Class to run tests for BaseTransformerBehaviour outside the three standard methods.
 
-    May need to overwite specific tests in this class if the tested transformer modifies this behaviour.
+    May need to overwrite specific tests in this class if the tested transformer modifies this behaviour.
     """
 
     @classmethod
